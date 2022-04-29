87702630969d0398914de3e2477583db2d7f27dd
AJ-DJ <94147359+k8omegaboi@users.noreply.github.com>
2022-04-29 17:52:13 -0500

    Add files via upload

---
+++ temper.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+root=Tk()
+
+root.title("Driving License")
+root.geometry("400x400")
+
+root.mainloop()
+
+ID = label_id
+name = label_name
+dob = label_dob
+pin = label_pin
+address = label_address
+vt = label_vehicle_type
+date_of_birth = random
+def DL():
+    DLID = 1234567890
+    print(vt)
+    print(dob)
+    print(pin)
+    print(address)
+    VT = ["two wheeler" , "four wheeler"]
+    print(VT)
+
+    PIN = 123456789000
+    label_address = random
+    label_id = DLID
+    label_dob = DOB
+    label_pin = PIN
+    label_vehicle_type = VT
+    
+    btn = Button(root, text="press me",command=DL)
+    label_dob.pack()
+    label_vehicle_type.pack()
+    label_address.pack()
+    label_pin.pack()
+    label_name.pack()
+    label_id.pack()
+    
+    
+    
+    
+    
+    
